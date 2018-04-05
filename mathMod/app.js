7993ca5e9f53eb1d2018a196911c97c87eaeb13f
JFLin <35427835+jlin728@users.noreply.github.com>
2018-04-05 18:58:44 -0400

    Add files via upload

---
+++ mathMod/app.js
@@ -0,0 +1,6 @@
+var mathlib = require('./mathlib')();     //notice the extra invocation parentheses!
+// console.log(mathlib);
+mathlib.add(23, 3333);
+mathlib.multiply(5,6);
+mathlib.square(5);
+mathlib.random(0,6);
